8f706a2699a89df40e9d484bf4f19559fd80c44c
Yara Haddad <99yarahaddad@gmail.com>
2024-10-12 22:23:12 +0300

    home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserModule } from './user/user.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => UserModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
