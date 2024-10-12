14216d3d5cd55d286a686def3211f0785b21bf69
Yara Haddad <99yarahaddad@gmail.com>
2024-10-12 21:10:42 +0300

    shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSharedModule } from './user/user-shared/user-shared.module';
+import { AdminSharedModule } from './admin/admin-shared/admin-shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserSharedModule,
+    AdminSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,13 +5,17 @@ import { UserRoutingModule } from './user-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { NavbarComponent } from './userShared/navbar/navbar.component';
+import { FooterComponent } from './userShared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     AboutComponent,
-    ContactusComponent
+    ContactusComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
