a42d95962c5ba23f66dd76590159e1d94e21baee
Yara Haddad <99yarahaddad@gmail.com>
2024-10-13 14:45:37 +0300

    login/reg html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => UserModule
+  },
+  {
+    path: 'security',
+    loadChildren: () => AuthModule
   }
 ];
 
