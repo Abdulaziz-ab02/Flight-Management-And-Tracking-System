8c95fe485fccd5584a02ef03a6dfc480456f9db6
Abdulazizjo <Abdulaziz_2k2@outlook.com>
2024-10-13 14:51:03 +0300

    routing for app module componants done :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserModule } from './user/user.module';
+import { HomeComponent } from './user/home/home.component';
+import { AboutComponent } from './user/about/about.component';
+import { ContactusComponent } from './user/contactus/contactus.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => UserModule
+    
+  },
+  
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'contactus',
+    component:ContactusComponent
   }
 ];
 
