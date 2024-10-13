6aec485f7b68b3c32db87d3f59243d3a917ea851
Abdulazizjo <Abdulaziz_2k2@outlook.com>
2024-10-13 21:00:33 +0300

    modified

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,9 +10,19 @@ import { Router } from '@angular/router'; // Import the Router
 export class LoginComponent {
   username: string = '';
   password: string = '';
-  rememberMe: boolean = false;
+  rememberMe:   boolean = false;
   constructor(private router: Router) {} // Inject Router service
+  ngOnInit() {
+    // Check if there is saved data in local storage
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
 
+    if (storedUsername && storedPassword) {
+      this.username = storedUsername;
+      this.password = storedPassword;
+      this.rememberMe = true;  // Set 'Remember me' checkbox to checked
+    }
+  }
   onSubmitLogin() {
     // Save the username and password in local storage
     if(this.rememberMe){
