58d7606a4d852e4a26f4d3d2532914f1367abe5e
leenalrashdan <lkalrashdan19@cit.just.edu.jo>
2024-10-18 00:43:59 +0300

    .

--- src/app/guest/guest-shared/guest-shared.module.ts
+++ src/app/guest/guest-shared/guest-shared.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -11,7 +14,16 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    MatButtonModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    HttpClientModule,
+    MatButtonModule
   ]
 })
 export class GuestSharedModule { }
