f227645bf35940e7b5874debd99a0d4b58466cd8
leenalrashdan <lkalrashdan19@cit.just.edu.jo>
2024-10-18 00:01:44 +0300

    create componet only

--- src/app/guest/guest.module.ts
+++ src/app/guest/guest.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GuestRoutingModule } from './guest-routing.module';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FlightsComponent } from './flights/flights.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutComponent,
+    ContactusComponent,
+    FlightsComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     GuestRoutingModule
