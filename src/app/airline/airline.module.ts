354b64fd8136a4ced90f18a55aba8364145649f7
leenalrashdan <lkalrashdan19@cit.just.edu.jo>
2024-10-25 23:02:32 +0300

    airline view&updateprofile

--- src/app/airline/airline.module.ts
+++ src/app/airline/airline.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AirlineRoutingModule } from './airline-routing.module';
 import { FlightsComponent } from './flights/flights.component';
 import { AdminSharedModule } from '../admin/admin-shared/admin-shared.module';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    FlightsComponent
+    FlightsComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
