8e0fa0b6b54fe3355e08aeb38b0235cf2fe07d66
leenalrashdan <lkalrashdan19@cit.just.edu.jo>
2024-11-12 06:52:41 +0300

    update auth. done

--- src/app/guard-name.guard.ts
+++ src/app/guard-name.guard.ts
@@ -7,25 +7,47 @@ export const guardNameGuard: CanActivateFn = (route, state) => {
   let toastr:ToastrService= inject(ToastrService);
   const token = localStorage.getItem('token');
   console.log(state);
-  if(token){
-if(state.url.indexOf('admin')>0){
-let user: any = localStorage.getItem('user'); 
-user = JSON.parse(user);
-if(user.roleid=='1'){
-  toastr.success('Welcome to Admin dashboard');
-  return true;
-}
-else{
-  toastr.warning('This page for Admin module')
-  router.navigate(['security/login']);
-  return false;
-}
+  if (token) {
+    let user: any = localStorage.getItem('user');
+    if (user) {
+      user = JSON.parse(user);  
 
-}
-return false;
-  }
-  else{
-    toastr.warning('Please sign up')
+      if (state.url.indexOf('admin') > 0) {
+        if (user.roleid === '1') {
+          toastr.success('Welcome to Admin dashboard');
+          return true;
+        } else {
+          toastr.warning('This page is for Admin module');
+          router.navigate(['security/login']);
+          return false;
+        }
+      }
+
+      if (state.url.indexOf('user') > 0) {
+        if (user.roleid === '2') {
+          toastr.success('Welcome to User dashboard');
+          return true;
+        } else {
+          toastr.warning('This page is for User module only');
+          router.navigate(['security/login']);
+          return false;
+        }
+      }
+
+      if (state.url.indexOf('airline') > 0) {
+        if (user.roleid === '3') {
+          toastr.success('Welcome to Airline dashboard');
+          return true;
+        } else {
+          toastr.warning('This page is for Airline module only');
+          router.navigate(['security/login']);
+          return false;
+        }
+      }
+    }
+    return false;
+  } else {
+    toastr.warning('Please sign up');
     router.navigate(['security/register']);
     return false;
   }
