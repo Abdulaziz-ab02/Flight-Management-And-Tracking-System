de7ba9d8efe65719726ac1df676922dbeb9eb02e
sadeen fadel <sadeenfadel@gmail.com>
2024-11-14 10:38:05 +0300

    0_0

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -16,6 +16,7 @@ import { CityComponent } from './city/city.component';
 import { CreateCityComponent } from './create-city/create-city.component';
 import { DegreeComponent } from './degree/degree.component';
 import { FacilitiesComponent } from './facilities/facilities.component';
+import { DegreeFacilityComponent } from './degree-facility/degree-facility.component';
 
 const routes: Routes = [
 
@@ -83,6 +84,14 @@ const routes: Routes = [
 {
   path:'Degree',
   component:DegreeComponent
+},
+{
+  path:'facilities',
+  component:FacilitiesComponent
+},
+{
+  path:'DegreeFacility',
+  component:DegreeFacilityComponent
 }
 ];
 
