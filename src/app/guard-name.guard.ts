b0ec5de7de80b3a9b29b931f9ef4a062d411f7b4
Yara Haddad <99yarahaddad@gmail.com>
2024-11-14 22:32:38 +0300

    url view profile auth

--- src/app/guard-name.guard.ts
+++ src/app/guard-name.guard.ts
@@ -6,16 +6,32 @@ export const guardNameGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   let toastr: ToastrService = inject(ToastrService);
   const token = localStorage.getItem('token');
-  
+
 
   const messageShown = sessionStorage.getItem('messageShown');
-  
+
   if (token) {
     let user: any = localStorage.getItem('user');
     if (user) {
       user = JSON.parse(user);
 
-     
+
+      if (state.url.includes('admin/viewprofile')) {
+        if (user.roleid === '1' || user.roleid === '3') {
+          if (!messageShown) {
+            toastr.success('Welcome to the Profile page');
+            sessionStorage.setItem('messageShown', 'true');
+          }
+          return true;
+        } else {
+          toastr.warning('This page is restricted');
+          router.navigate(['security/login']);
+          return false;
+        }
+      }
+
+
+
       if (state.url.indexOf('admin') > 0) {
         if (user.roleid === '1') {
           if (!messageShown) {
